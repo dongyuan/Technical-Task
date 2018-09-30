7178d50d98b848f60327e47014a3462f511f8e70
Eric Yuan <mbaeric@gmail.com>
2018-09-30 08:38:39 +0100

    Initial Commit

---
+++ FlickrGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrGallery
+//
+//  Created by Eric Yuan on 30/09/2018.
+//  Copyright © 2018 Code Challenge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
